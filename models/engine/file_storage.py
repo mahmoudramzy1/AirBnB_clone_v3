a525c54cf302cf2a558ac9f041811ff84173acb0
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2024-06-08 08:19:19 -0700

    l

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,17 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """get the object with id given"""
+        obj_dict = self.all(cls)
+        matchkey = cls + '.' + id
+        for key in obj_dict:
+            if key == matchkey:
+                return obj_dict[key]
+        return None
+
+    def count(self, cls=None):
+        """count number of objects in class or all"""
+        obj_dict = self.all(cls)
+        return len(obj_dict)
