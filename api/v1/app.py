0e4074bb1b8d61a5e3f5b63b2435112a8c21d2f5
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-09 19:45:42 +0300

    Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,8 +3,11 @@
 
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
+import os
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
@@ -15,8 +18,6 @@ def tear_down(e):
 
 
 if __name__ == "__main__":
-    app.run(
-            host=app.config.get('HBNB_API_HOST', '0.0.0.0'),
-            port=int(app.config.get('HBNB_API_PORT', '5000')),
-            threaded=True
-            )
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = os.getenv("HBNB_API_PORT", 5000)
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 """ Index """
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models import storage
+
 from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route("/status")
 def status():
-    """ Status of API """
+    """retrun status json"""
     return jsonify({"status": "OK"})
