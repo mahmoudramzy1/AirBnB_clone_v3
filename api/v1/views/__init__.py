2d9c47dde9c90875ba7750793838ee2d27d3dde0
mahmoudramzy1 <mahmoud.ahmed.ramzy@outlook.com>
2024-06-09 19:07:18 +0300

    p

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 from flask import Blueprint
+from api.v1.views.index import *
 """make blueprint of app_view"""
 
-
 app_views = Blueprint('app', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
