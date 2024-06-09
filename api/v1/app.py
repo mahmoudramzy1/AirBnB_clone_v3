05496824bc588a1eb3cc9652ebf58dd9a91bac98
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-09 20:09:48 +0300

    Handle 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,12 @@ def tear_down(e):
     storage.close()
 
 
+@app.errorhandler(404)
+def error_hande(e):
+    """return json 404"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", 5000)
