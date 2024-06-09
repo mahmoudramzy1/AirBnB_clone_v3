711f0d5c0089146043ed726e7175f27a0df76438
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-09 19:59:22 +0300

    Add states route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,21 @@ from flask import jsonify
 def status():
     """retrun status json"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """return stats"""
+    from models import storage
+    from models.state import State
+    from models.city import City
+    from models.amenity import Amenity
+    from models.place import Place
+    from models.review import Review
+    from models.user import User
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User}
+    count = {}
+    for key, value in classes.items():
+        count[key] = storage.count(value)
+    return jsonify(count)
