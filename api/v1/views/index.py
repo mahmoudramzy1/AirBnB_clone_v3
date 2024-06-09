9eb1291b9b30606e2004d42d19b090f86fb33729
root <mahmoud.ahmed.ramzy@outlook.com>
2024-06-09 15:35:45 +0000

    p

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,8 @@
-
+import app_views
 """first status"""
 
 
 @app_views.route('/status')
 def status():
     """return status"""
-    from api.v1.views import app_views
     return { "status": "OK" }
