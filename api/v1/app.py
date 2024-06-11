70879498797d3723b6b76eafc95fb422ba25534b
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-11 05:58:58 +0300

    Use CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,13 @@
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def tear_down(e):
