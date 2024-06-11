d121c61351339524b70ddf8e92c7149baa6f533a
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-11 07:35:48 +0300

    use get_json

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -37,9 +37,9 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Create a new state object"""
-    if not request.json:
+    if not request.get_json():
         abort(400, 'Not a JSON')
-    if 'name' not in request.json:
+    if 'name' not in request.get_json():
         abort(400, 'Missing name')
     state = State(**request.get_json())
     state.save()
@@ -52,7 +52,7 @@ def update_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if not request.json:
+    if not request.get_json():
         abort(400, 'Not a JSON')
     for key, value in request.get_json().items():
         if key not in ['id', 'created_at', 'updated_at']:
