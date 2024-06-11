2058b3a467a0e37fd27fb7c308e8b88632fa6259
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-11 04:44:54 +0300

    Fixing the route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -23,7 +23,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/api/v1/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
     """Delete a state object"""
     state = storage.get(State, state_id)
